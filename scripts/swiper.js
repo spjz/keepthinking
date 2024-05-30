8aa5fe355feb565304b98084225937efe1ecc127
Joe Doherty <j.doherty@keepthinking.it>
2024-05-30 17:32:36 +0100

    Restructured project for frontend demo

---
+++ scripts/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-dots',
+    },
+});
---
+++ scripts/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'display': ['DINPro Bold'],
+                'body': ['DINPro Regular']
+            },
+            colors: {
+                clifford: '#da373d',
+            }
+        }
+    }
+}
